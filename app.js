574a6819838ccf33af9667bcfcff798f3f155a59
henry blas <hblas2014@gmail.com>
2023-05-10 22:31:53 -0500

    se realizo ejemplos de BOM

---
+++ app.js
@@ -0,0 +1,18 @@
+//prompt
+window.alert('esta es una alerta');
+window.prompt('hola)');
+const question = window.confirm('confirmar');
+console.log(question);
+const link = document.querySelector('a');
+const menu = document.getElementById('menu-navegation');
+console.log(menu);
+
+const size = window.screen;
+
+console.log(size);
+if size === Screen {availWidth: 1920, availHeight: 1032, width: 1920, height: 1080, colorDepth: 24, …} {
+    console.log('tipo de dispositivo')
+}
+    
+console.log(size.with);
+console.log(size.heigt);
